f52b84cb97217d16f5737348f228e3c3a0f800b8
tej singh chaudhary <tej.bijay@gmail.com>
2021-08-23 23:42:34 +0545

    added material ui and some icons

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,20 +1,28 @@
 import "./topbar.scss"
+import {Person,Mail} from "@material-ui/icons"
 
 export default function Topbar() {
     return (
-        <div className="topbar">
+        <div className="topbar">            
             <div className="wrapper">
                 <div className="left">
+                        <a href="#intro" className = "home" >Home</a> 
 
-                        <a href="#intro">Intro</a>
                         
+                     
+                        <div className="itemContainer">
+                            <Person className="icon"/>
+                           <span>9808928400</span>
+                            </div> 
 
+                        <div className="itemContainer">
+                            <Mail className="icon"/>
+                            <span>tej.bijay@gmail.com</span>
+                            
+                            </div>                 
                 </div>
-
-                <div className="right">
-
-
-                </div>
+                        <div className="right">
+                        </div>
 
             </div>
          
