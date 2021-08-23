890951bb752249d83849f65c0d6573d54c2014ff
tej singh chaudhary <tej.bijay@gmail.com>
2021-08-23 15:06:59 +0545

    added link to the top

--- src/components/testomonials/Testomonials.jsx
+++ src/components/testomonials/Testomonials.jsx
@@ -2,7 +2,7 @@ import "./testomonials.scss"
 
 export default function Testomonials() {
     return (
-        <div className="testomonials">
+        <div className="testomonials"id= "testomonials">
           
         </div>
     )
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,6 +3,20 @@ import "./topbar.scss"
 export default function Topbar() {
     return (
         <div className="topbar">
+            <div className="wrapper">
+                <div className="left">
+
+                        <a href="#intro">Intro</a>
+                        
+
+                </div>
+
+                <div className="right">
+
+
+                </div>
+
+            </div>
          
         </div>
     )
